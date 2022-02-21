c2bdd9b9d7fb6cb3dbc9bf3446ca4fd70a78a18b
andohikaru <dev0319h@gmail.com>
2022-02-21 22:55:57 +0900

    Initial Commit

---
+++ QiitaArticleListSwiftUI/QiitaArticleListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaArticleListSwiftUIApp.swift
+//  QiitaArticleListSwiftUI
+//
+//  Created by AndoHikaru on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct QiitaArticleListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
